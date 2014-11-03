1014c5de43aef855e8196db29e1a100b998807e6
Richard Harrington <richardharrington@gmail.com>
2014-11-03 07:33:58 -0600

    first commit

---
+++ src/solver/Solver.java
@@ -0,0 +1,26 @@
+package solver;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Solver{
+
+	
+	class Element{
+		char letter;
+		int value;
+	}
+	private int tests, lines;
+	
+	public static void main(String args[]){
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("input.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
